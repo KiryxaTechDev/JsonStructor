4fa87d2ada2ee1f095489c6d3f944138cc71d1f7
KiryxaTechDev <kiryxatech@gmail.com>
2024-06-18 02:05:43 +0500

    Added 'update_key' method to 'JsonFile' class.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 :copyright: (c) 2024 KiryxaTech
 """
 
-version = "0.0.1"
+version = "0.0.2"
 
 with open('README.md', encoding='utf-8') as f:
     long_discription = f.read()
